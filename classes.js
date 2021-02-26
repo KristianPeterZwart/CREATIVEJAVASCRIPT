45684b879330cc3ec687e3122518dd239fccda4b
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-26 17:13:24 +0100

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,10 +1,17 @@
 
-function Enemy(life, name, level) {
-    this.life = life;
-    this.name = name;
-    this.level = level;
+class Enemy {
+    constructor(life, name, level){
+        this.life = life;
+        this.name = name;
+        this.level = level;
+    }
+getInfo() {
+        console.log(this.life, this.name, this.level);
+    }
 }
 
-Enemy.prototype.getInfo = function() {
-    console.log(this.life, this.name, this.level);
-};
+const turtle = new Enemy(25, "Tommy", 10);
+const bug = new Enemy(1, "Bobby", 1);
+
+turtle.getInfo();
+bug.getInfo();
